cfe773b350757039ad528c35d5713979dd777f2b
betelihem-26 <bethelhemkiros27@gmail.com>
2024-10-09 23:08:17 -0400

    row

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import Banner from "../../Components/Banner/banner"
+import RowLst from '../../Components/Rows/RowList/RowLst';
 
-// import RowList from "../../Components/Rows/RowList/RowList";
 
 const Home = () => {
   return (
     <>
       <Header/>
       <Banner/>
-      {/* <RowList/> */}
+      <RowLst/>
       <Footer/>
     </>
   )
