8f27785e8c3b2fad6a90b646b338def6d9ada6b0
betelihem-26 <bethelhemkiros27@gmail.com>
2024-10-15 06:19:57 -0400

    updated project

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Netflix-Clone-2024",
+  base: "/Netflix-Clone-2024/", 
   plugins: [react()],
 });
